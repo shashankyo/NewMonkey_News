501aab9480e281fce8ee7683a4b3106b5ffedc9f
shashank <shashank944533@gmail.com>
2023-04-30 06:37:14 +0530

    News monkey complete project

---
+++ sampleOutput.js
@@ -0,0 +1,19 @@
+{
+    "status"; "ok",
+    "totalResults"; 1
+    "articles"; [
+        {
+            "source": {
+                "id": null,
+                "name": "Yahoo Entertainment"
+            },
+            "author": "Matt Harmon",
+            "title": "2023 NFL Draft Care/Don't Care: Where do Bryce Young, C.J. Stroud fit the best? - Yahoo Sports",
+            "description": "Fantasy football analyst Matt Harmon delivers a special NFL draft edition of his Care/Don't Care column!",
+            "url": "https://sports.yahoo.com/2023-nfl-draft-caredont-care-where-do-bryce-young-cj-stroud-fit-the-best-213459776.html",
+            "urlToImage": "https://s.yimg.com/ny/api/res/1.2/UhP_AFBwOOXc3iH9NhmL4w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2023-04/bfbd2320-df98-11ed-9f8c-7703cf77c13c",
+            "publishedAt": "2023-04-26T21:34:59Z",
+            "content": "5 things I care about\r\nWhere the top QBs land\r\nQuarterbacks are the talk of the town every draft season. This year, we have four guys for sure going in Round 1 (and we might even sneak in a fifth). I… [+13591 chars]"
+        }
+    ]
+}
